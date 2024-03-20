05e32ae8028ac0ba93b6d53372a2def40e6d9a3f
shirlyan <57712324+shirlyan@users.noreply.github.com>
2024-03-20 09:59:10 +0800

    Add files via upload

---
+++ 2. customers-who-never-order.py
@@ -0,0 +1,75 @@
+'''
+Customers 表：
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+在 SQL 中，id 是该表的主键。
+该表的每一行都表示客户的 ID 和名称。
+
+Orders 表：
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+在 SQL 中，id 是该表的主键。
+customerId 是 Customers 表中 ID 的外键( Pandas 中的连接键)。
+该表的每一行都表示订单的 ID 和订购该订单的客户的 ID。
+ 
+
+找出所有从不点任何东西的顾客。
+以 任意顺序 返回结果表。
+结果格式如下所示。
+
+示例 1：
+输入：
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+输出：
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+'''
+import pandas as pd
+
+column = ['id', 'name']
+data = [[1, 'Joe'], [2, 'Henry'], [3, 'Sam'], [4, 'Max']]
+customers = pd.DataFrame(data, columns=column)
+
+column = ['id', 'customerId']
+data = [[1, 3], [2, 1]]
+orders = pd.DataFrame(data, columns=column)
+
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    res = pd.merge(customers, orders, left_on='id',
+                   right_on='customerId', how='left')
+    res1 = res[res.isnull()['customerId']][['name']]
+    # res1 = res1.rename(columns=['Customers'], inplace=True)
+    res1.columns = ['Customers']
+    print(res1)
+    return res1
+
+
+find_customers(customers, orders)
